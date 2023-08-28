6d4ffa8fdcb0c4a30275b767218cbd994d4f7c80
sj2532nb <sj2532nb@gmail.com>
2023-08-28 21:58:29 +0900

    Updates

--- src/components/wrap/RightModalComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RightModalComponent(){
-    return (
-        <div id="rightmodal">
-            <a href="!#">FOUNDATION FACTORY</a>
-        </div>
-    );
-};
